4ae1159b6a759b04c2a7d57d6a0f54e71fd8181e
Matthew Waller <waller.matthew@gmail.com>
2016-02-15 11:06:12 -0600

    Initial Commit

---
+++ MyOnTheMapTests/MyOnTheMapTests.swift
@@ -0,0 +1,36 @@
+//
+//  MyOnTheMapTests.swift
+//  MyOnTheMapTests
+//
+//  Created by Matthew Waller on 2/15/16.
+//  Copyright © 2016 Matthew Waller. All rights reserved.
+//
+
+import XCTest
+@testable import MyOnTheMap
+
+class MyOnTheMapTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
